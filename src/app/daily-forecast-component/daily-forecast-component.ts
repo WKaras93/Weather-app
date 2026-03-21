84585d5cf355c561206685d10424ce87c0424433
Wojtek <wojciechkaras1993@gmail.com>
2026-03-21 14:59:53 +0100

    feat: add service facade to daily forecast component

--- src/app/daily-forecast-component/daily-forecast-component.ts
+++ src/app/daily-forecast-component/daily-forecast-component.ts
@@ -1,54 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { OpenMeteoService, DailyForecastResponse } from '../services/open-meteo/open-meteo-service';
-import { Coordinates, GeolocationService, LocationName } from '../services/geolocation/geolocation-service';
 import { WeatherIconService } from '../services/weather-icon/weather-icon-service';
-import { map } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { DailyForecast } from '../models/daily-forecast.model';
+import { WeatherFacadeService } from '../services/weather-facade/weather-facade.service';
+import { TemperatureDisplayPipe } from '../pipes/temperature-display.pipe';
 
 @Component({
   selector: 'app-daily-forecast-component',
-  imports: [],
+  imports: [ TemperatureDisplayPipe ],
   templateUrl: './daily-forecast-component.html',
   styleUrl: './daily-forecast-component.less'
 })
 export class DailyForecastComponent implements OnInit {
     dailyForecasts: DailyForecast[] = [];
 
+    private destroy$ = new Subject<void>();
+
     constructor(
-        private _locationService: GeolocationService,
-        private _openMeteoService: OpenMeteoService,
-        private _weatherIconService: WeatherIconService
+        private _weatherIconService: WeatherIconService,
+        private _weatherFacadeService: WeatherFacadeService
     ) {}
 
     ngOnInit() {
-        this._locationService.getCurrentLocation().subscribe(
-            ([coords, locationName]: [Coordinates, LocationName]) => {
-                this._openMeteoService
-                    .getDailyForecast(coords.latitude, coords.longitude)
-                    .pipe(
-                        map((weatherResponse: DailyForecastResponse) =>
-                            weatherResponse.daily.time.map((_, i) => ({
-                                date: weatherResponse.daily.time[i],
-                                minTemperature: Math.round(weatherResponse.daily.temperature_2m_min[i]),
-                                maxTemperature: Math.round(weatherResponse.daily.temperature_2m_max[i]),
-                                forecastCode: weatherResponse.daily.weather_code[i],
-                                day: this.getDay(weatherResponse.daily.time[i])
-                            }))
-                        )
-                    )
-                .subscribe((dailyForecasts) => {
-                    this.dailyForecasts = dailyForecasts
-                })
-            }
-        );
+        this._weatherFacadeService.dailyForecasts$
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(dailyForecastResponse => this.dailyForecasts = dailyForecastResponse!);
     }
 
     public getWeatherIcon(weatherCode: number): string {
         return this._weatherIconService.getMappedWeatherIcon(weatherCode);
-    }
-
-    private getDay(date: string): string {
-        const _date = new Date(date);
-        return (new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(_date));
     }
 }
